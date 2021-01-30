318dc3d8f6ce358e51f88fd96bb8fe501e746eac
djKooks <inylove82@gmail.com>
2021-01-30 16:35:33 +0900

    add basic commands

--- kleine.py
+++ kleine.py
@@ -1,5 +1,12 @@
 
 import click
+from kafka import KafkaProducer, KafkaConsumer, TopicPartition
+
+from kafka.cluster import ClusterMetadata
+
+KAFKA_HOST = 'localhost:9092'
+TEST_TOPIC = 'test'
+
 
 @click.group()
 def cli():
@@ -7,11 +14,30 @@ def cli():
 
 
 @cli.command()
-def test():
+def produce():
+    '''
+    produce message
     '''
     
-    '''
-    print('test click')
+    producer = KafkaProducer(bootstrap_servers=KAFKA_HOST)
+    for i in range(2):
+        producer.send(TEST_TOPIC, b'some_bytes')
 
+
+@cli.command()
+def consume():
+    '''
+    consume message
+    '''
+    consumer = KafkaConsumer(bootstrap_servers=KAFKA_HOST)
+    consumer.assign([TopicPartition(TEST_TOPIC, 0)])
+    for msg in consumer:
+        print (msg)
+
+
+@cli.command()
+def cluster():
+    cluster = ClusterMetadata(bootstrap_servers=KAFKA_HOST)
+    print(cluster.brokers())
 
 
